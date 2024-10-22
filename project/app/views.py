cd6dc5cc3917d192e0a3c3dcba78a86265c4b344
rishadroshanpt <rishadroshan27778@gmail.com>
2024-10-22 11:56:46 +0530

    task completed

--- project/app/views.py
+++ project/app/views.py
@@ -28,3 +28,37 @@ def task3(req,num):
         return HttpResponse('The last digit of the number is divisible by 3.')
     else:
         return HttpResponse('The last digit of the number is not divisible by 3.')
+def task4(req,ch):
+    if ch==1:
+        return HttpResponse('Sunday')
+    elif ch==2:
+        return HttpResponse('Monday')
+    elif ch==3:
+        return HttpResponse('Tuesday')
+    elif ch==4:
+        return HttpResponse('Wednesday')
+    elif ch==5:
+        return HttpResponse('Thursday')
+    elif ch==6:
+        return HttpResponse('Friday')
+    elif ch==7:
+        return HttpResponse('Saturday')
+    else:
+        return HttpResponse('Invalid choice !')
+def task5(req,price):
+    if price>100000:
+        tax=price/100*15
+    elif price>50000 and price<=100000:
+        tax=price/100*10
+    else:
+        tax=price/100*5
+    return HttpResponse(tax)
+def task6(req,units):
+    if units<=100:
+        return HttpResponse('No charge')
+    elif units>100 and units<=200:
+        charge=(units-100)*5
+        return HttpResponse(charge)
+    else:
+        charge=(units-200)*10
+        return HttpResponse(charge+500)
--- project/project/urls.py
+++ project/project/urls.py
@@ -25,8 +25,9 @@ urlpatterns = [
     path('fun3/<a>',views.fun3),
     path('task1/<int:salary>/<int:year>',views.task1),
     path('task2/<city>',views.task2),
-    path('task3/<int:num>',views.task3)
-
-
+    path('task3/<int:num>',views.task3),
+    path('task4/<int:ch>',views.task4),
+    path('task5/<int:price>',views.task5),
+    path('task6/<int:units>',views.task6)
 
 ]
