8b74ae8566c61579395ddcac8fe71c0bb0b48f5b
rishadroshanpt <rishadroshan27778@gmail.com>
2024-11-04 23:14:04 +0530

    admin side completed

---
+++ eshop/app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('',views.shp_login),
+    path('shp_logout',views.shp_logout),
+    path('shp_home',views.shp_home),
+    path('add_prod',views.add_prod),
+    path('edit_prod/<pid>',views.edit_prod),
+    path('delete_prod/<pid>',views.delete_prod),
+
+
+]
