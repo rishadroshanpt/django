3a883cf3434bb37033a7b3226fcc1bdc99dc089c
rishadroshanpt <rishadroshan27778@gmail.com>
2024-11-27 21:20:12 +0530

    h

--- synnefo/app/views.py
+++ synnefo/app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def index(req):
-    return render(req,'index.html')
+    data=Course.objects.all()
+    return render(req,'index.html',{'data':data})
+def course(req,id):
+    data=Course.objects.get(pk=id)
+    return render(req,'course.html',{'data':data})
